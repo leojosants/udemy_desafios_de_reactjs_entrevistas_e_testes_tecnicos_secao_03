9fcd12e120084ca2c53fdd489e8635c16de4feaa
Leonardo Santos <leonardojosantos@gmail.com>
2023-09-25 19:29:34 -0300

    class 25 - exercise 7: using solution state

---
+++ estado_e_ciclo_de_vida/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Toggle from './components/Toggle';
+
+function App() {
+  return (
+    <>
+      <h2>1 - Usando o Estado</h2>
+      <Toggle />
+    </>
+  );
+}
+
+export default App;
